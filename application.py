2d05432fd1cab2d02b4535585210015635e87317
road-pig <qiuzeeming@gmail.com>
2023-03-16 10:39:10 +0800

    transferred some file

--- application.py
+++ application.py
@@ -5,23 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():  # put application's code here
-    return render_template('index.html', name=' to Law Database')
+    return render_template('index.html', page="index", name=' to Law Database')
 
 
 @app.route('/<string:name>')
 def index_name(name):
-    return render_template('index.html', name=f', {name}')
+    return render_template('index.html', page="index", name=f', {name}')
 
 
 @app.route('/feedback')
 def feedback():
-    return render_template('feedback.html')
+    return render_template('feedback.html', page="feedback")
 
 
 @app.route('/database')
 def database():
-    return render_template('database.html')
+    return render_template('database.html', page="database")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
